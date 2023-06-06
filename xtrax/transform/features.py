8fe53631cbcc2af63f7d0940b2d58b9d4489e775
José Medeiros <joseprsm@gmail.com>
2023-06-06 01:05:49 +0100

    fix encoders

--- xtrax/transform/features.py
+++ xtrax/transform/features.py
@@ -5,25 +5,30 @@ from .min_max import MinMaxScaler
 from .one_hot import OneHotEncoder
 
 
-class BaseEncoder(tuple):
-    def __new__(cls, transformer, feature_index: list[int], name: str = None):
-        name = name or cls.__name__
-        return tuple.__new__(cls, (name, transformer, feature_index))
+class BaseEncoder:
+    def __init__(self, transformer, feature_index: list[int], name: str = None):
+        self._name = name or self.__class__.__name__
+        self._transformer = transformer
+        self._feature_index = feature_index
+
+    def __iter__(self):
+        for x in [self._name, self._transformer, self._feature_index]:
+            yield x
 
 
 class CategoryEncoder(BaseEncoder):
-    def __new__(cls, feature_index: list[int], name: str = None):
+    def __init__(self, feature_index: list[int], name: str = None):
         pipeline = make_pipeline(OrdinalEncoder(), OneHotEncoder())
-        return super().__new__(pipeline, feature_index, name)
+        return super().__init__(pipeline, feature_index, name)
 
 
 class NumberEncoder(BaseEncoder):
-    def __new__(cls, feature_index: list[int], name: str = None):
+    def __init__(self, feature_index: list[int], name: str = None):
         transformer = MinMaxScaler(feature_range=(-1, 1))
-        return super().__new__(transformer, feature_index, name)
+        return super().__init__(transformer, feature_index, name)
 
 
 class IDEncoder(BaseEncoder):
-    def __new__(cls, feature_index: list[int], name: str = None):
+    def __init__(cls, feature_index: list[int], name: str = None):
         transformer = OrdinalEncoder()
-        return super().__new__(transformer, feature_index, name)
+        return super().__init__(transformer, feature_index, name)
