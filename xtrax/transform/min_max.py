7f8ca8fc9acbcb2b3af66441015813f533662c8c
José Medeiros <joseprsm@gmail.com>
2023-06-06 01:05:43 +0100

    back to roots

--- xtrax/transform/min_max.py
+++ xtrax/transform/min_max.py
@@ -4,21 +4,18 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class MinMaxScaler(BaseEstimator, TransformerMixin):
+
+    _min: int
+    _max: int
+
     def __init__(self, feature_range=None):
-        super().__init__()
         self.feature_range = feature_range or (0, 1)
 
     def fit(self, X, *_):
-        data_min = jnp.min(X, axis=0)
-        data_max = jnp.max(X, axis=0)
-
-        diff = data_max - data_min
-        self._scale = (self.feature_range[1] - self.feature_range[0]) / diff
-        self._min = self.feature_range[0] - data_min * self._scale
+        self._min = jnp.min(X, axis=0)
         self._max = jnp.max(X, axis=0)
         return self
 
     def transform(self, X):
-        X *= self._scale
-        X += self._min
+        X = (X - self._min) / (self._max - self._min)
         return X
