9f981357f4f24a03bd692de10f900108782717bb
José Medeiros <joseprsm@gmail.com>
2023-08-02 00:10:20 +0100

    data to float

--- nectarine/transform/encode/min_max.py
+++ nectarine/transform/encode/min_max.py
@@ -10,10 +10,10 @@ class MinMaxScaler(BaseEstimator, TransformerMixin):
         self.feature_range = feature_range or (0, 1)
 
     def fit(self, X, *_):
+        X = X.astype(float)
         self._min = jnp.min(X, axis=0)
         self._max = jnp.max(X, axis=0)
         return self
 
     def transform(self, X):
-        X = (X - self._min) / (self._max - self._min)
-        return X
+        return (X.astype(float) - self._min) / (self._max - self._min)
