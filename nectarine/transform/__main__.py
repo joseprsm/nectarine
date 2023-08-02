2c7600370c088c7d323db236eec68f016cd14bb4
José Medeiros <joseprsm@gmail.com>
2023-08-02 18:59:33 +0100

    add sample transform module

---
+++ nectarine/transform/__main__.py
@@ -0,0 +1,23 @@
+import json
+
+import click
+import pandas as pd
+
+from nectarine import Extractor
+
+
+@click.command()
+@click.option("--users-path", "users", required=True)
+@click.option("--items-path", "items", required=True)
+@click.option("--schema-path", "schema", required=True)
+@click.option("--interactions-path", "interactions", required=True)
+def transform(interactions, users, items, schema):
+    with open(schema, "r") as f:
+        schema = json.load(f)
+    interactions = pd.read_csv(interactions)
+    extractor = Extractor(schema, users, items)
+    features = extractor.fit(interactions).transform(interactions)  # noqa: F841
+
+
+if __name__ == "__main__":
+    transform()
