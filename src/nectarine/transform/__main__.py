6f5f5af36ae997f311ee3c1f2853fc0ca2b5ce35
José Medeiros <joseprsm@gmail.com>
2023-08-08 00:08:17 +0100

    add outputs

--- src/nectarine/transform/__main__.py
+++ src/nectarine/transform/__main__.py
@@ -11,7 +11,16 @@ from nectarine import Extractor
 @click.option("--items-path", "--items", "items", required=True)
 @click.option("--schema-path", "--schema", "schema", required=True)
 @click.option("--interactions-path", "--interactions", "interactions", required=True)
-def transform(interactions, users, items, schema):
+@click.option("--transform-layer", "interactions")
+@click.option("--model-config", "interactions")
+def transform(
+    interactions,
+    users,
+    items,
+    schema,
+    transform_layer: str = None,
+    model_config: str = None,
+):
     with open(schema, "r") as f:
         schema = json.load(f)
     interactions = pd.read_csv(interactions)
