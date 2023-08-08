6f2e106c5e2b65462ef0ba13ba7ab0f757b16bd6
José Medeiros <joseprsm@gmail.com>
2023-08-09 00:35:02 +0100

    add base workflow test

---
+++ tests/test_workflow.py
@@ -0,0 +1,10 @@
+from nectarine.train.__main__ import train
+from nectarine.transform.__main__ import transform
+
+
+transform = transform.callback
+train = train.callback
+
+
+def test_workflow():
+    pass
