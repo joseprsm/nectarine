460e4037f5a5f590a8ea5ce45749454efc20365f
José Medeiros <joseprsm@gmail.com>
2023-08-08 01:20:40 +0100

    input transform layer to model

--- nectarine/train/recommender.py
+++ nectarine/train/recommender.py
@@ -1,18 +1,21 @@
 from flax import linen as nn
 from jax import numpy as jnp
 
+from ..transform import TransformOutput
 from .tower import Tower
 
 
 class Recommender(nn.Module):
     schema: dict
     config: dict
+    transform: TransformOutput
 
     def setup(self):
         self.query_model = Tower(**self.config["query"])
         self.candidate_model = Tower(**self.config["candidate"])
 
-    def __call__(self, user: jnp.ndarray, item: jnp.ndarray):
+    def __call__(self, user_id: jnp.ndarray, item_id: jnp.ndarray):
+        user, item = self.transform(user_id, item_id)
         query_embeddings = self.query_model(**user)
         candidate_embeddings = self.candidate_model(**item)
         return query_embeddings, candidate_embeddings
--- nectarine/transform/__init__.py
+++ nectarine/transform/__init__.py
@@ -1,2 +1,3 @@
 from .extract import Extractor
+from .output import TransformOutput
 from .transform import FeatureTransformer
