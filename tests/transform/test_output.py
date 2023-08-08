3e3a3a5b1c23a5e762fa593f570794782eea2789
José Medeiros <joseprsm@gmail.com>
2023-08-08 16:46:09 +0100

    add base test for TransformOutput

---
+++ tests/transform/test_output.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pytest
+
+from nectarine.transform import TransformOutput
+
+
+@pytest.mark.parametrize(
+    ["users", "items", "user_ids", "item_ids"],
+    [
+        (
+            np.random.randint(100, size=90).reshape(-1, 3),
+            np.random.randint(100, size=90).reshape(-1, 3),
+            np.random.randint(89, size=10),
+            np.random.randint(89, size=10),
+        )
+    ],
+)
+def test_transform_output(users, items, user_ids, item_ids):
+    TransformOutput(users, items).apply({}, user_ids, item_ids)
