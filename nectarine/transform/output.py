7e7d78aff3193a89c7be4c8da6224de07ae09e2f
José Medeiros <joseprsm@gmail.com>
2023-08-08 16:55:57 +0100

    add __repr__

--- nectarine/transform/output.py
+++ nectarine/transform/output.py
@@ -8,7 +8,9 @@ class TransformOutput(nn.Module):
     items: jnp.ndarray
 
     @nn.compact
-    def __call__(self, user_id: jnp.ndarray, item_id: jnp.ndarray):
+    def __call__(
+        self, user_id: jnp.ndarray, item_id: jnp.ndarray
+    ) -> tuple[jnp.ndarray, jnp.ndarray]:
         user_features = self._get_features(user_id, self.users)
         item_features = self._get_features(item_id, self.items)
         # todo: include context features
@@ -16,10 +18,13 @@ class TransformOutput(nn.Module):
 
     @staticmethod
     @jax.jit
-    def _get_features(ids: jnp.ndarray, data: jnp.ndarray):
+    def _get_features(ids: jnp.ndarray, data: jnp.ndarray) -> jnp.ndarray:
         ids = ids.reshape(-1, 1)
         features = jnp.take(data, ids, axis=0)
         # todo: confirm features shape is always (1, 1, ..., n)
         features = features.reshape(-1, features.shape[-1])
         features = jnp.concatenate([ids, features], axis=1)
         return features
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(users={self.users.shape}, items={self.items.shape})"
