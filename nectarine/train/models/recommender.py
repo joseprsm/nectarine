1fd377fde42d20a6b584af2ae9e10f6731b29df5
José Medeiros <joseprsm@gmail.com>
2023-10-03 00:35:30 +0100

    update docstring

--- nectarine/train/models/recommender.py
+++ nectarine/train/models/recommender.py
@@ -10,14 +10,15 @@ class Recommender(tfrs.Model):
     It expects a `tf.data.Dataset`, composed of two keys: "query" and "candidate";
     the query part of the dataset has three keys:
 
-    * the user ID feature name, a scalar;
-    * `user_features`, an array representing the user features
-    * `context_features`, an array representing the context features
+    * `id`, a scalar;
+    * `features`, an array representing the user features
+    * `session`, an array representing the previously interacted items
+    * `context`, an array representing the context features
 
     The candidate part of the data set has two keys:
 
-    * the item ID feature name, a scalar;
-    * `item_features`, an array representing the item features
+    * `id`, a scalar;
+    * `features`, an array representing the item features
 
     The query tower model takes the user ID feature and passes it by an embedding layer. The
     user and context features are concatenated and passed by a number of dense layers. The
@@ -25,24 +26,32 @@ class Recommender(tfrs.Model):
     to the outputs of the features model whose inputs are the item features, and are then
     passed by a number of Dense layers.
 
-    An optional Ranking model is also included, granted there are `ranking_features`.
-
     Args:
-        user_dims (int): number possible values for the user ID feature
-        item_dims (int): number possible values for the item ID feature
-        embedding_dim (int): output dimension of the embedding layer
-        feature_layers (list): number of neurons in each layer for the feature models
-        output_layers (list): number of neurons in each layer for the output models
+        config (dict): number possible values for the user ID feature
 
     Examples:
-        >>> from rexify.models import Recommender
-        >>> model = Recommender()
+        >>> import tensorflow as tf
+        >>> inputs = tf.data.Dataset.from_tensor_slices(
+                {
+                    "query": {
+                        "id": [[1], [2], [3]],
+                        "session": [[1, 2, 3], [2, 3, 4], [3, 4, 5]],
+                        "features": [[1.0, 0.8], [1.0, 0.4], [1.0, 0.2]],
+                    },
+                    "candidate": {
+                        "id": [[1], [2], [3]],
+                        "features": [[1.0, 0.8], [1.0, 0.4], [1.0, 0.2]],
+                    },
+                },
+            )
+        >>> config = {
+                "query": {"n_users": 4, "n_items": 5, "embedding_dim": 5},
+                "candidate": {"n_items": 3, "embedding_dim": 5},
+            }
+        >>> from nectarine import Recommender
+        >>> model = Recommender(config=config)
         >>> model.compile()
-
-        >>> import numpy as np
-        >>> inputs = tf.data.Dataset.from_tensor_slices(np.concatenate([np.random.randint(0, 15, size=100).reshape(-1, 1), np.random.randint(0, 1, size=100).reshape(-1, 1), np.random.randint(0, 1_000, size=100).reshape(-1, 1), np.random.randint(0, 1_000, size=100).reshape(-1, 1), np.random.randint(0, 15, size=100).reshape(-1, 1), np.random.randint(0, 5, size=100).reshape(-1, 1),], axis=1)).map(lambda x: {'query': {'user_id': x[0], 'user_features': x[1:3], 'context_features': x[3:4]}, 'candidate': {'item_id': x[4], 'item_features': x[5:]}}).batch(128)
-
-        >>> _ = model.fit(inputs, verbose=0)
+        >>> model.fit(inputs, batch_size=3, epochs=2, verbose=0)
 
     """
 
