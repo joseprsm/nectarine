ddaa9b67cb190ff57548ca607d4800a6d7cf6472
José Medeiros <joseprsm@gmail.com>
2023-11-02 23:58:43 +0000

    update transform script

--- nectarine/transform/__main__.py
+++ nectarine/transform/__main__.py
@@ -2,21 +2,23 @@ import click
 import pandas as pd
 import yaml
 
-from nectarine import Transform
+from .._util import get_dataset
+from ..transform import Transform
 
 
 @click.command()
 @click.option("--data", required=True)
-@click.option("--schema", required=True)
+@click.option("--config", required=True)
 @click.option("--target")
-def transform(data: str, schema: str, target: str):
-    with open(schema, "r") as s:
-        schema = yaml.load(s)
+def transform(data: str, config: str, target: str):
+    with open(config, "r") as s:
+        config = yaml.safe_load(s)
 
-    with Transform(schema, target) as trf:
-        data: pd.DataFrame = trf.download(data)
-        features = trf(data)
-        features.upload(target)
+    with Transform(config, target) as transform:
+        dataset: dict = get_dataset(config, "transform", target)
+        read: callable = getattr(pd, f"read_{dataset['format']}")
+        data: pd.DataFrame = read(data)
+        ids, features = transform(data)
 
 
 if __name__ == "__main__":
