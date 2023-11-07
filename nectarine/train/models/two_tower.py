b44bcb5bb2e69f95c5df3cf7d574278de82638b3
José Medeiros <joseprsm@gmail.com>
2023-11-07 20:38:05 +0000

    add retrieval task

--- nectarine/train/models/two_tower.py
+++ nectarine/train/models/two_tower.py
@@ -1,5 +1,7 @@
+import jax
 import jax.numpy as jnp
 from flax import linen as nn
+from flax.training.train_state import TrainState
 
 from ..layers.tower import CandidateTower, QueryTower
 
@@ -15,3 +17,27 @@ class TwoTower(nn.Module):
         user_embeddings = QueryTower(**self.config["query"])(user)
         item_embeddings = CandidateTower(**self.config["candidate"])(item)
         return user_embeddings, item_embeddings
+
+    @staticmethod
+    @jax.jit
+    def retrieval(state: TrainState, user: features, item: features):
+        def categorical_crossentropy(y_true, y_pred):
+            epsilon = 1e-8
+            y_pred = jnp.clip(y_pred, a_min=-10, a_max=10)
+            log_probs = jax.nn.log_softmax(y_pred, axis=-1)
+            loss = -jnp.sum(y_true * (log_probs + epsilon))
+            return jnp.mean(loss)
+
+        def loss_fn(params):
+            embeddings = state.apply_fn({"params": params}, user, item)
+            query_embeddings, candidate_embeddings = tuple(map(jnp.squeeze, embeddings))
+            scores = jnp.matmul(query_embeddings, jnp.transpose(candidate_embeddings))
+            n_queries, n_candidates, *_ = scores.shape
+
+            labels = jnp.eye(n_queries, n_candidates)
+            loss = categorical_crossentropy(labels, scores)
+            return loss
+
+        grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
+        loss, grads = grad_fn(state.params)
+        return grads, loss
