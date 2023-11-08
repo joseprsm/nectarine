059df56237508f89542dd60774e927a20df38658
José Medeiros <joseprsm@gmail.com>
2023-11-08 22:28:07 +0000

    improve towers modularity

--- nectarine/train/layers/tower.py
+++ nectarine/train/layers/tower.py
@@ -1,43 +1,30 @@
 import jax.numpy as jnp
 from flax import linen as nn
 
+from .features import FeaturesModel
+from .output import OutputModel
+
 
 class _Tower(nn.Module):
     n_dims: int
-    embedding_dim: int = 16
-    feature_layers: list[int] = None
-    output_layers: list[int] = None
-
-    def feature_model(self, features, activation: str = None):
-        x = features
-        for layer in self.feature_layers:
-            x = nn.Dense(layer)(x)
-            if activation:
-                x = getattr(nn, activation)(x)
-        return x
-
-    def output(self, *args):
-        x = jnp.concatenate(args, axis=1)
-        x = nn.LayerNorm()(x)
-        for layer in self.output_layers[:-1]:
-            x = nn.Dense(layer)(x)
-            x = nn.LayerNorm()(x)
-            x = nn.relu(x)
-        x = nn.Dense(self.output_layers[-1])(x)
-        return x
+    embedding_dim: int
+    feature_layers: list[int]
+    output_layers: list[int]
 
 
 class QueryTower(_Tower):
     @nn.compact
     def __call__(self, inputs: dict[str, jnp.ndarray], *_):
         x = nn.Embed(self.n_dims, self.embedding_dim)(inputs["id"])
-        features = self.feature_model(inputs["features"], "leaky_relu")
-        return self.output(x, features)
+        features = FeaturesModel(self.feature_layers, "leaky_relu")(inputs["features"])
+        x = OutputModel(self.output_layers)(x, features)
+        return x
 
 
 class CandidateTower(_Tower):
     @nn.compact
     def __call__(self, inputs: dict[str, jnp.ndarray], *_):
         x = nn.Embed(self.n_dims, self.embedding_dim)(inputs["id"])
-        features = self.feature_model(inputs["features"], "leaky_relu")
-        return self.output(x, features)
+        features = FeaturesModel(self.feature_layers, "leaky_relu")(inputs["features"])
+        x = OutputModel(self.output_layers)(x, features)
+        return x
