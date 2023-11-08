d5a48c650f474696bbce2c4d3789da1a364f954f
José Medeiros <joseprsm@gmail.com>
2023-11-08 23:59:14 +0000

    fix set params

--- nectarine/train/_train.py
+++ nectarine/train/_train.py
@@ -12,8 +12,12 @@ from flax.training.train_state import TrainState
 from nectarine.train.models import TwoTower
 
 
+NUM_EPOCHS = 10
+LEARNING_RATE = 0.01
+BATCH_SIZE = 32
+
 rng = jax.random.PRNGKey(0)
-rng, init_rng = jax.random.split(rng)
+rng, INIT_RNG = jax.random.split(rng)
 
 
 class Train:
@@ -22,31 +26,44 @@ class Train:
     state: TrainState
 
     def __init__(self, config: dict) -> None:
-        self._config: dict[str, dict] = config["train"]
+        self.config = config["train"].copy()
+        self.params = config.pop("hyperparameters")
 
     def __enter__(self):
-        self.params = ...  # todo: validate parameters
-        self.model = self._set_model(self._config)
-        self.state = self._create_train_state()
         return self
 
     def __call__(
         self,
         training_data,
     ):
-        for epoch in range(1, self.params["epochs"] + 1):
-            state, train_loss = self._train_epoch(training_data)
+        def get_param(value: str):
+            return (
+                self.params[value]
+                if value in self.params.keys()
+                else globals()[value.upper()]
+            )
+
+        epochs = get_param("num_epochs")
+        lr = get_param("learning_rate")
+        batch_size = get_param("batch_size")
+
+        self.model = self._set_model(self.config["model"], self.params)
+
+        self.state = self._init(self.model, lr)
+        for epoch in range(1, epochs + 1):
+            self.state, train_loss = self._train_epoch(training_data, batch_size)
             print(f"epoch: {epoch}, train_loss: {train_loss}")
-        return state
+
+        return self.state
 
     def __exit__(self, *_):
         pass
 
-    def _train_epoch(self, data: pd.DataFrame):
-        steps_per_epoch = data.shape[0] // self.params["batch_size"]
+    def _train_epoch(self, data: pd.DataFrame, batch_size: int):
+        steps_per_epoch = data.shape[0] // batch_size
         batches = jax.random.permutation(rng, data.shape[0])
-        batches = batches[: steps_per_epoch * self.params["batch_size"]]
-        batches = batches.reshape((steps_per_epoch, self.params["batch_size"]))
+        batches = batches[: steps_per_epoch * batch_size]
+        batches = batches.reshape((steps_per_epoch, batch_size))
 
         epoch_loss = []
 
@@ -59,19 +76,24 @@ class Train:
         train_loss = jnp.mean(jnp.array(epoch_loss))
         return state, train_loss
 
-    def create_train_state(rng, model: nn.Module, learning_rate: float = 0.01):
-        inputs = ...  # todo: get sample inputs
-        params = model.init(rng, **inputs)["params"]
+    @jax.jit
+    def _init(self, learning_rate: float = 0.01):
+        inputs = {
+            "query": {"id": jnp.zeros((5, 1))},
+            "candidate": {"id": jnp.zeros((5, 1))},
+        }
+        params = self.model.init(INIT_RNG, **inputs)["params"]
         tx = optax.adamw(learning_rate)
-        return TrainState.create(apply_fn=model.apply, params=params, tx=tx)
+        return TrainState.create(apply_fn=self.model.apply, params=params, tx=tx)
 
     @staticmethod
-    def _set_model(config: dict):
+    def _set_model(model_name: str, params: dict) -> nn.Module:
         def is_module(x):
             return "flax.linen.module.Module" in str(x[1].__base__)
 
-        model_config: dict = ...  # todo: get model config
-        module = importlib.import_module(f"nectarine.train.models.{config['model']}")
+        module = f"nectarine.train.models.{model_name}"
+        module = importlib.import_module(module)
+
         model = filter(is_module, inspect.getmembers(module, inspect.isclass))
-        model = next(model)[1](model_config)
+        model = next(model)[1](**params)
         return model
