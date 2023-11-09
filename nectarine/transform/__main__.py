2c16ba72afee8d4e7bf13bf29f1caff5a27fbe59
José Medeiros <joseprsm@gmail.com>
2023-11-09 23:09:13 +0000

    remove click dependency

--- nectarine/transform/__main__.py
+++ nectarine/transform/__main__.py
@@ -1,19 +1,24 @@
-import click
+import argparse
+
 import pandas as pd
 
 from ..transform import Transform
 
 
-@click.command()
-@click.option("--config", required=True)
-@click.option("--target")
-def transform(config: str, target: str):
-    with Transform(config, target) as transform:
-        dataset: dict = transform.get_dataset(target=target)
-        read: callable = getattr(pd, f"read_{dataset['format']}")
-        data: pd.DataFrame = read(dataset["location"])
-        transform(data)
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", "-c", required=True)
+    parser.add_argument("--target", "-t", required=True)
+    args = parser.parse_args()
+    return vars(args)
 
 
 if __name__ == "__main__":
-    transform()
+    args = get_args()
+    config, target, output = args.values()
+
+    with Transform(config, target) as transform:
+        dataset: dict = transform.get_dataset()
+        read: callable = getattr(pd, f"read_{dataset['format']}")
+        data: pd.DataFrame = read(dataset["location"])
+        transform(data)
